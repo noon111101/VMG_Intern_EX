1480c9c5103769e79d19a82d0b50324e6e17e9d4
noon111101 <trieu11112001@gmail.com>
2022-09-13 23:48:56 +0700

    feat bai6

---
+++ Bai6_Spring3/Thực hành 1/src/main/java/com/ThucHanh.java
@@ -0,0 +1,10 @@
+package com.asbnotebook
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucHanh {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ThucHanh.class,args);
+    }
+}
